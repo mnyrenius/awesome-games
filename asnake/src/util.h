fdddd78928eae8404851509a0202207e0147fb18
Mats Nyrenius <mnyrenius@gmail.com>
2023-10-04 21:20:57 +0200

    asnake: Log only in debug build

--- asnake/src/util.h
+++ asnake/src/util.h
@@ -5,7 +5,12 @@
 
 void check_gl_errors(const char *file, int line);
 
+#ifdef DEBUG
 #define CheckGlErrors() check_gl_errors(__FILE__, __LINE__)
 #define LOG(fmt, ...) printf("[%s:%d]: " fmt, __FILE__, __LINE__, __VA_ARGS__)
+#else
+#define CheckGlErrors()
+#define LOG(fmt, ...)
+#endif
 
 #endif /* _UTIL_H_ */
